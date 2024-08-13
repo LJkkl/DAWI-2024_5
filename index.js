2039787126dfe7875c726df3b36371b2b0b88451
LJkkl <luandossantosjardim7@gmail.com>
2024-08-13 14:52:45 -0300

    adicionei mensagens

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express()
 app.get('/', function(req,res){
     res.send("Oi mundo!")
 })
-app.get('/diego', function(req,res){
-    res.send("Oi diego!")
+app.get('/luan', function(req,res){
+    res.send("Oi Luan!")
+})
+app.get('/:x', function(req,res){
+    const mensagem = "Oi" + req.params.x
+    res.send()
+})
+app.get('/:nome/:sobrenome', function(req,res){
+    const mensagem = "Oi"+req.params.nome + " "+req.params.sobrenome
+    res.send(mensagem)
 })
 
 
